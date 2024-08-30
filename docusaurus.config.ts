8d8902baf2d40f84dd08e40070df2d44c540520e
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2024-08-31 00:21:37 +0200

    PUSH -> Docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -143,6 +143,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['php']
     },
   } satisfies Preset.ThemeConfig,
   plugins: [
